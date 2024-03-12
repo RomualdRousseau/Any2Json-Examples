b28cee2dddaf83488a1c39219eaac8b0552d7175
Romuald Rousseau <romuald.rousseau@servier.com>
2024-03-12 16:20:02 +0800

    Add tutorial

---
+++ src/main/java/com/github/romualdrousseau/any2json/examples/Tutorial6.java
@@ -0,0 +1,45 @@
+package com.github.romualdrousseau.any2json.examples;
+
+import java.util.EnumSet;
+import java.util.List;
+
+import com.github.romualdrousseau.any2json.Document;
+import com.github.romualdrousseau.any2json.DocumentFactory;
+import com.github.romualdrousseau.any2json.parser.LayexTableParser;
+
+public class Tutorial6 implements Runnable {
+
+    public Tutorial6() {
+    }
+
+    @Override
+    public void run() {
+        final var model = Common.loadModelFromGitHub("sales-english");
+
+        // Add a layex to the model
+
+        final var tableParser = new LayexTableParser(
+                List.of("(v.$)+"),
+                List.of("(()(E+$E+$))(()(/^PRODUCTCODE/.+$)*(/PRODUCTCODE/.+$))+()"));
+        model.registerTableParser(tableParser);
+
+        final var file = Common.loadData("document with noises.xls", this.getClass());
+        try (final var doc = DocumentFactory.createInstance(file, "UTF-8")
+                .setModel(model)
+                .setHints(EnumSet.of(Document.Hint.INTELLI_LAYOUT))
+                .setRecipe(
+                    "sheet.setCapillarityThreshold(1.5)",
+                        "sheet.setDataTableParserFactory(\"DataTableGroupSubFooterParserFactory\")",
+                        "sheet.dropRowsWhenFillRatioLessThan(0.2)")) {
+
+            doc.sheets().forEach(s -> Common.addSheetDebugger(s).getTable().ifPresent(t -> {
+                Common.printHeaders(t.headers());
+                Common.printRows(t.rows());
+            }));
+        }
+    }
+
+    public static void main(final String[] args) {
+        new Tutorial6().run();
+    }
+}
